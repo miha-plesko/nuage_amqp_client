68cbe4a3adcdd7c42f3756a636a9bb8564afb001
Gregor Berginc <gregor.berginc@gmail.com>
2017-09-28 03:22:06 +0000

    Simple Qpid-based AMQP client for Nuage message bus
    Signed-off-by: Gregor Berginc <gregor.berginc@gmail.com>

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem "qpid_proton", "~>0.17.0.1", :git => "https://github.com/gberginc/qpid_proton_gem"
---
+++ nuage_amqp_client.rb
@@ -0,0 +1,72 @@
+#--
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#++
+
+require 'qpid_proton'
+require 'optparse'
+
+class NuageMB < Qpid::Proton::Handler::MessagingHandler
+
+  def initialize(url, topic, opts)
+    super()
+    @url = url
+    @topic = topic
+    @opts = opts
+    @test_connection = @opts.delete(:test_connection)
+  end
+
+  def on_start(event)
+    event.container.container_id = "Ruby AMQP"
+    conn = event.container.connect(@url, @opts)
+    event.container.create_receiver(conn, :source => @topic)
+    puts "started"
+  end
+
+  def on_connection_opened(event)
+#    super
+    puts "opened"
+    event.container.stop if @test_connection
+  end
+
+  def on_connection_closed(event)
+    puts "closed"
+  end
+
+  def on_connection_error(event)
+    raise Exception("Connection error")
+  end
+
+  def on_message(event)
+    puts event.message.body
+    #event.connection.close
+  end
+
+  def on_transport_error(event)
+    raise "Connection error: #{event.transport.condition}"
+  end
+end
+
+options = {
+  :sasl_allowed_mechs        => "PLAIN", 
+  :sasl_allow_insecure_mechs => true,
+  :test_connection           => false}
+
+hw = NuageMB.new(ENV['NUAGE_AMQP'],
+                 "topic://topic/CNAMessages",
+                 options)
+Qpid::Proton::Reactor::Container.new(hw).run
